94a3397ed4fa7db99aadf8cbb96ba8bd625ba235
Samuel Bruin <67058024+Ancientkingg@users.noreply.github.com>
2020-10-07 09:36:36 +0200

    Update shortcut.js

--- shortcut.js
+++ shortcut.js
@@ -1,6 +1,6 @@
 const ws = require('windows-shortcuts');
 var folder = "C:\Users\Familie\Downloads"
-ws.create("%UserProfile%/Desktop/R6S Black Ice.lnk", {
+ws.create("%UserProfile%\\Desktop\\R6S Black Ice.lnk", {
     target: folder.split("\\").join("\\\\") + "\\R6DownloaderV4.2.exe",
     runStyle: ws.NORMAL,
     desc: "R6 Version made by R6Terminal",
